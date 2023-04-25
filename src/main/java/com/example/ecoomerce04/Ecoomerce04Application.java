2db73a3ad03fbb63e13ac6ae5a842d43a799a9a9
hyunWoo <nhw0926@gmail.com>
2023-04-25 21:55:49 +0900

    Initial commit

---
+++ src/main/java/com/example/ecoomerce04/Ecoomerce04Application.java
@@ -0,0 +1,13 @@
+package com.example.ecoomerce04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecoomerce04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecoomerce04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecoomerce04/Ecoomerce04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecoomerce04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecoomerce04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
